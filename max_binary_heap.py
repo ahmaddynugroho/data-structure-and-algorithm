ed3504a35ddfadbd63a8a863d74e0314383e8557
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-01-12 10:17:58 +0700

    feat: heap (remove max)

--- max_binary_heap.py
+++ max_binary_heap.py
@@ -19,6 +19,30 @@ class MaxBinaryHeap:
 
     def swap(self, idx_1, idx_2):
         self.items[idx_1], self.items[idx_2] = self.items[idx_2], self.items[idx_1]
+    
+    def remove_max(self):
+        if not self.items:
+            raise Exception("Heap is empty")
+        max_el = self.items[0]
+        end_idx = len(self.items) - 1
+        self.swap(0, end_idx)
+        self.items.pop()
+        self.move_down()
+        return max_el
+    
+    def move_down(self):
+        parent_idx = 0
+        child_idx = parent_idx * 2 + 1
+        end_idx = len(self.items) - 1
+        while child_idx <= end_idx:
+            if child_idx < end_idx and self.items[child_idx] <= self.items[child_idx + 1]:
+                child_idx += 1
+            if self.items[parent_idx] < self.items[child_idx]:
+                self.swap(parent_idx, child_idx)
+                parent_idx = child_idx
+                child_idx = parent_idx * 2 + 1
+            else:
+                break
 
 
 # %%
@@ -29,5 +53,6 @@ for x in n:
 
 heap.insert(46)
 heap.insert(30)
+heap.remove_max()
 
 heap.items
