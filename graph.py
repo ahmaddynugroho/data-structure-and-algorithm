dc34474b7b821d57ae22e8e14b1d34d361924a93
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-01-14 15:55:57 +0700

    fix: graph (add edge)

--- graph.py
+++ graph.py
@@ -14,7 +14,7 @@ class Graph:
             raise Exception("vertex does not exist")
         if vertex_2 not in self.adjacency_list[vertex_1]:
             self.adjacency_list[vertex_1].append(vertex_2)
-        if vertex_2 not in self.adjacency_list[vertex_1]:
+        if vertex_1 not in self.adjacency_list[vertex_2]:
             self.adjacency_list[vertex_2].append(vertex_1)
         return self
 
@@ -35,15 +35,17 @@ class Graph:
         self.adjacency_list.pop(vertex)
         return self
 
+    def bft(self):  # Breath First Traversal
+        pass
+
 
 # %%
 g = Graph()
+vertices = list("ABCDEFGHIJK")
+for v in vertices:
+    g.add_vertex(v)
+edges = "AB.AC.AD.BC.CD.CJ.DE.DI.EF.EG.FH.GH.GK.IJ.JK".split(".")
+for e in edges:
+    g.add_edge(e[0], e[1])
 
-g.add_vertex("A")
-g.add_vertex("B")
-
-g.add_edge("A", "B")
-g.add_edge("B", "A")
-
-g.remove_vertex("B")
 g.adjacency_list
