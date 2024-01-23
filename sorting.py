00781edff2f4525d6a4b512fc3f70141611fe395
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-01-23 11:37:38 +0700

    feat: sort (optimized bubble)

--- sorting.py
+++ sorting.py
@@ -20,9 +20,13 @@ def selection_sort(array):
 
 def bubble_sort(array):
     for i in range(len(array) - 1):
+        has_swapped = False
         for j in range(len(array) - 1, i, -1):
             if array[j - 1] > array[j]:
                 array[j - 1], array[j] = array[j], array[j - 1]
+                has_swapped = True
+        if not has_swapped:
+            break
     return array
 
 
