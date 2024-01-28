505ae3623d941fe44f54fbd29e3736e5676361a7
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-01-28 14:55:48 +0700

    feat: sort (heap sort)

--- sorting.py
+++ sorting.py
@@ -41,6 +41,35 @@ def shell_sort(array):
     return array
 
 
+def heapify(array):
+    parent_idx = len(array) // 2 - 1
+    end_idx = len(array) - 1
+    for idx in range(parent_idx, -1, -1):
+        move_down(array, idx, end_idx)
+    return array
+
+
+def move_down(array, start_idx, end_idx):
+    child_idx = start_idx * 2 + 1
+    while child_idx <= end_idx:
+        if child_idx < end_idx and array[child_idx] <= array[child_idx + 1]:
+            child_idx += 1
+        if array[start_idx] < array[child_idx]:
+            array[start_idx], array[child_idx] = array[child_idx], array[start_idx]
+            start_idx = child_idx
+            child_idx = start_idx * 2 - 1
+        else:
+            break
+
+
+def heap_sort(array):
+    heapify(array)
+    for i in range(len(array) - 1, 0, -1):
+        array[0], array[i] = array[i], array[0]
+        move_down(array, 0, i - 1)
+    return array
+
+
 # %%
 ar = [17, 59, 23, 70, 11, 42, 10, 31, 95, 20]
-shell_sort(ar)
+heap_sort(ar)
